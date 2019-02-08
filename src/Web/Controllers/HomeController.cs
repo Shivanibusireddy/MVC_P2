c73174ed978b4d9bba0af8d93960b0822004c277
shivani busireddy <shivanireddy30@gmail.com>
2019-02-08 12:32:39 -0600

    Added new Action to the Controllers/HomeController.cs and updated the menu

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -19,6 +19,11 @@ namespace Web.Controllers
         {
             return View();
         }
+        public IActionResult Showview()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
