153542d784117be08efc9a5dddb96abc809c8dc3
shivani busireddy <shivanireddy30@gmail.com>
2019-02-08 14:08:02 -0600

    Created a Test1 function

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -8,7 +8,38 @@ namespace UnitTests
         [Fact]
         public void Test1()
         {
+            var outcome = 105;
+            var a = 7;
+            var actual = OddNum(a);
+            Assert.Equal(outcome, actual);
+        }
 
+        public void Test2()
+        {
+            var expected = 3;
+                var a = -3;
+            var actual = OddNum(a);
+            Assert.Equal(expected, actual);
+               
+        }
+        int OddNum(int x)
+        {
+            var b = 1;
+            var pro = 1;
+            if (x < 0)
+            {
+                x = -x;
+            }
+            for (b = 1; b <= x; b++)
+            {
+                if (b % 2 != 0)
+                {
+                    pro *= b;
+                }
+
+
+            }
+            return pro;
         }
     }
 }
